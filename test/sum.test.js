bf86938ba0bcfd091e5357f77a195b011f082b98
Peranikov <ym.nekoze@gmail.com>
2017-02-27 11:05:16 +0900

    Hello power-assert

--- test/sum.test.js
+++ test/sum.test.js
@@ -1,5 +1,18 @@
 import sum from '../src/sum';
+const assert = require('assert');
 
-test('adds 1 + 2 to equal 3', () => {
+test('jest success', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('jest fail', () => {
+  expect(sum(1, 2)).toBe(4);
+});
+
+test('power-assert success', () => {
+  assert.equal(sum(1, 2), 3);
+})
+
+test('power-assert fail', () => {
+  assert.equal(sum(1, 2), 6);
+})
