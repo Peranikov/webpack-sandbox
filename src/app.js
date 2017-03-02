d7c601acfa8833c40549632407efad919ba7ee7a
Peranikov <ym.nekoze@gmail.com>
2017-03-02 18:31:40 +0900

    Hello flow!

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
+import { sum } from "./sum"
 import cats from "./cats.json"
+
 console.log(cats)
+
+console.log(sum(1,2))
 
 const arrow = () => {
   console.log("Arrow function")
--- test/sum.test.js
+++ test/sum.test.js
@@ -1,18 +1,10 @@
-import sum from '../src/sum';
+import {sum} from '../src/sum';
 const assert = require('assert');
-
-test('jest success', () => {
-  expect(sum(1, 2)).toBe(3);
-});
-
-test('jest fail', () => {
-  expect(sum(1, 2)).toBe(4);
-});
 
 test('power-assert success', () => {
   assert.equal(sum(1, 2), 3);
 })
 
-test('power-assert fail', () => {
-  assert.equal(sum(1, 2), 6);
+test('power-assert success', () => {
+  assert.equal(sum(1, -1), 0);
 })
