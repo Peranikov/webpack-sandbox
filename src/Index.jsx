2c2a1c633fa7d244d7615e4103e1195d34885a7e
Peranikov <ym.nekoze@gmail.com>
2017-03-12 00:31:38 +0900

    Add redux-logger

--- src/Index.jsx
+++ src/Index.jsx
@@ -4,16 +4,21 @@ import "babel-polyfill"
 
 /*eslint-disable no-unused-vars*/
 import React from 'react'
-import { createStore } from 'redux'
+import { applyMiddleware, createStore } from 'redux'
 import { Provider } from 'react-redux'
 import { render } from 'react-dom'
 import App from './components/App'
 import reducer from './reducers'
+import createLogger from 'redux-logger'
 
 import font_awesome from 'font-awesome/css/font-awesome.css'
 /*eslint-enable no-unused-vars*/
 
-const store = createStore(reducer)
+const logger = createLogger()
+const store = createStore(
+  reducer,
+  applyMiddleware(logger)
+)
 
 render(
   <Provider store={store}>
