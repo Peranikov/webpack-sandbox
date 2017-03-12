6addd8efb2ad1deeff17623a2f6c72288014d132
Peranikov <ym.nekoze@gmail.com>
2017-03-12 21:15:45 +0900

    Add initial monster data

--- src/reducers/monsters.js
+++ src/reducers/monsters.js
@@ -2,8 +2,17 @@ import Monster from "../models/Monster"
 
 const initialState = [
   new Monster({ id: 1, name: "ゴブリン", atack: 1}),
-  new Monster({ id: 2, name: "オーク", atack: 3}),
-  new Monster({ id: 3, name: "バンパイア", atack: 4, type: "undead"}),
+  new Monster({ id: 2, name: "ゴブリン", atack: 1}),
+  new Monster({ id: 3, name: "スケルトン", atack: 2, type: "undead"}),
+  new Monster({ id: 4, name: "スケルトン", atack: 2, type: "undead"}),
+  new Monster({ id: 5, name: "オーク", atack: 3}),
+  new Monster({ id: 6, name: "オーク", atack: 3}),
+  new Monster({ id: 7, name: "バンパイア", atack: 4, type: "undead"}),
+  new Monster({ id: 8, name: "バンパイア", atack: 4, type: "undead"}),
+  new Monster({ id: 9, name: "ゴーレム", atack: 5}),
+  new Monster({ id: 10, name: "ゴーレム", atack: 5}),
+  new Monster({ id: 11, name: "リッチ", atack: 5, type: "undead"}),
+  new Monster({ id: 12, name: "デーモン", atack: 7}),
   new Monster({ id: 13, name: "ドラゴン", atack: 9, type: "dragon"}),
 ]
 
