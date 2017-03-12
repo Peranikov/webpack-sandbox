f03b07e91e5b8d39850ddfec42d504fe1b99b463
Peranikov <ym.nekoze@gmail.com>
2017-03-12 15:37:57 +0900

    Add HitPoint remain style

--- src/components/HitPointBar.jsx
+++ src/components/HitPointBar.jsx
@@ -1,14 +1,33 @@
 /*eslint-disable no-unused-vars*/
 import React, { PropTypes } from 'react'
+import CSS from '../styles/app.css'
 /*eslint-enable no-unused-vars*/
 
-const HitPointBar = ({ hitPoint, sumAtack }) => (
-  <div>
-    <span>
-      HP: {hitPoint} - {sumAtack} = {hitPoint - sumAtack}
-    </span>
-  </div>
-)
+const HitPointBar = ({ hitPoint, sumAtack }) => {
+  const remainHitPoint = hitPoint - sumAtack
+  const remainHitPointCss = (() => {
+    if (remainHitPoint > 0) {
+      return CSS.hitPointPlus
+    } else {
+      return CSS.hitPointMinus
+    }
+  })()
+
+  return(
+    <div>
+      HP:
+      <span style={{ padding: 10 }}>
+        {hitPoint}
+      </span>
+      -
+      <span style={{ padding: 10 }}>
+        {sumAtack}
+      </span>
+      =
+      <span style={{ padding: 10 }} className={remainHitPointCss}>{remainHitPoint}</span>
+    </div>
+  )
+}
 
 HitPointBar.propTypes = {
   hitPoint: PropTypes.number.isRequired,
